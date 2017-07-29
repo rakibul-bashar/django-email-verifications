bc9fb5e5f34be8b01659b50f3ff37d8574324738
rakibul-bashar <rakibulbasharrakib@gmail.com>
2017-07-30 01:24:44 +0600

    updated

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rakibulbasharrakib@gmail.com'
-EMAIL_HOST_PASSWORD = 'rakib5500'
+EMAIL_HOST_USER = 'yourgmailaccount@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_PORT = 587
 import os
 
